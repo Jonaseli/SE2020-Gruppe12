916264d0069b731b330c8802c67edc85dbd7e84a
Jonas Eliassen <jonas.sarpsborg@gmail.com>
2020-10-22 23:37:06 +0200

    Removed capital letters from vue component names, caused vue to not work correctly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,30 +5,29 @@ import repository.Repository;
 
 public class Main {
     public static void main(String[] args){
+
         Javalin app = Javalin.create().start(7000);
         app.config.enableWebjars();
-        String[] filesPaths = new String[4];
 
+        String[] filesPaths = new String[4];
         filesPaths[0] = "parkingSpots.json";
         filesPaths[1] = "accounts.json";
         filesPaths[2] = "posts.json";
         filesPaths[3] = "reservations.json";
 
-        app.before("/", ctx -> ctx.redirect("/login"));
 
-        app.get("/login", new VueComponent("loginPage"));
-
+        app.get("/", new VueComponent("login-page"));
 
         //Vue
         //Får opp blank side, så vue fungerer, lurer på om det er lurt å separate repo objekter for hver import
         //dersom vi ikke får inn all info på samme side
-        app.get("/parkingSpot", new VueComponent("parkingSpots-overview"));
+        //app.get("/parkingSpot", new VueComponent("parkingSpots-overview"));
 
         Repository repo = new Repository(filesPaths);
 
         //api
-        Controller parkingSpotController = new Controller(repo);
-        app.get("api/parkingSpot", parkingSpotController :: getAllSpots);
+        //Controller parkingSpotController = new Controller(repo);
+        //app.get("api/parkingSpot", parkingSpotController :: getAllSpots);
 
     }
 
---
+++ src/main/resources/vue/views/login-page.vue
@@ -0,0 +1,10 @@
+<template id ="login-test">
+    <h1 class="hello">Hello, javalin!</h1>
+</template>
+<script>
+    Vue.component("login-page",
+        {   template : "#login-test"
+        })
+</script>
+<style>
+</style>
