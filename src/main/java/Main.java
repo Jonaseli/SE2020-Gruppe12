8b3faa7f0577c85ed352f61c4b0a86331fef6403
Jonas Eliassen <jonas.sarpsborg@gmail.com>
2020-10-22 13:04:42 +0200

    Added dependencies needed for vue to work.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,17 @@ import repository.Repository;
 public class Main {
     public static void main(String[] args){
         Javalin app = Javalin.create().start(7000);
-
         app.config.enableWebjars();
-
         String[] filesPaths = new String[4];
 
         filesPaths[0] = "parkingSpots.json";
         filesPaths[1] = "accounts.json";
         filesPaths[2] = "posts.json";
         filesPaths[3] = "reservations.json";
+
+        app.before("/", ctx -> ctx.redirect("/login"));
+
+        app.get("/login", new VueComponent("loginPage"));
 
 
         //Vue
---
+++ src/main/resources/vue/views/loginPage.vue
@@ -0,0 +1,16 @@
+<template id="loginPage">
+    <div>
+        <h1>LoginPage</h1>
+    </div>
+</template>
+<script>
+    Vue.component("loginPage", {
+    template: "#loginPage"
+    });
+</script>
+<style>
+   body{
+       background-color: #000000;
+       max-width: 1800px;
+   }
+</style>
