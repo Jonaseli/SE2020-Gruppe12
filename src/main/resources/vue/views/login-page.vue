c677feaf44b5b48c3fcd23a994005a65e238782d
Jonas Eliassen <jonas.sarpsborg@gmail.com>
2020-11-15 12:25:28 +0100

    Restructure of login page so its testable

--- src/main/resources/vue/views/login-page.vue
+++ src/main/resources/vue/views/login-page.vue
@@ -1,37 +1,8 @@
-<template id ="login">
-    <div>
-        <header>
-            <h1>Login Page</h1>
-        </header>
-        <main>
-            <button id="userBtn" v-on:click="redirect" class="center">Login as User</button>
-            <button id="corpBtn" v-on:click="redirect" class="center">Login as Corporation</button>
-            <button id="adminBtn" v-on:click="redirect" class="center">Login as Admin</button>
-        </main>
-    </div>
+<template>
+    <script type="module" src="components/login-buttons.vue"></script>
 </template>
 <script>
-    //import Vue from 'vue'
-    //window.Vue = require('vue');
-    console.log(window);
-    Vue.component("login-page",{
-        template : "#login",
-        methods: {
-            redirect: function (event) {
-                window.location.href = 'parking-spot';
-            }
-        }});
+    Vue.component("login-page",{template : "#login-buttons"});
 </script>
 <style>
-    header {
-        text-align: center;
-    }
-
-    main {
-        text-align: center;
-    }
-
-    main, button {
-        height: 50px;
-    }
 </style>
